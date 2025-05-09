806f10cd5d54ef0435ce581544c8140105ce92f2
Narjissi-fadwa <fadwa16narjissi@gmail.com>
2025-05-09 15:22:14 +0100

    gallery + team  + responsive + hero-carrousel

--- public/js/app.js
+++ public/js/app.js
@@ -77,3 +77,73 @@ function filterItems() {
 }
 
 filterButtons.forEach(btn => btn.addEventListener("click", filterItems));
+
+
+//! Carousel 
+let nextBtns = document.querySelectorAll(".next-btn")
+let previousBtns = document.querySelectorAll(".previous-btn")
+let containers = document.querySelectorAll(".carousel-container")
+
+let carouselIndexes = {}
+
+for (let i = 0; i < nextBtns.length; i++) {
+    nextBtns[i].addEventListener("click", function (e) {
+        let target = this.getAttribute("carousel-target")
+        let targetID = 0
+        if (carouselIndexes[target] !== undefined) {
+            targetID = carouselIndexes[target]
+        }
+        slider(targetID + 1, target)
+    })
+}
+
+for (let i = 0; i < previousBtns.length; i++) {
+    previousBtns[i].addEventListener("click", function (e) {
+        let target = this.getAttribute("carousel-target")
+        let targetID = 0
+        if (carouselIndexes[target] !== undefined) {
+            targetID = carouselIndexes[target]
+        }
+        slider(targetID - 1, target)
+    })
+}
+
+function slider(index, target) {
+    for (let i = 0; i < containers.length; i++) {
+        let container = containers[i]
+        if (container.id == target) {
+            let camera = container.querySelector(".carousel")
+            let slides = container.querySelectorAll(".slide")
+            if (slides.length === 0) {
+                return
+            }
+            let slideWidth = slides[0].offsetWidth
+            if (!slideWidth) {
+                slideWidth = slides[0].clientWidth
+            }
+            if (index < 0) {
+                index = slides.length - 1
+            } else if (index >= slides.length) {
+                index = 0
+            }
+            camera.style.transform = "translateX(-" + (slideWidth * index) + "px)"
+            carouselIndexes[target] = index
+        }
+    }
+}
+
+let showCarouselBtn = document.getElementById("show-carousel")
+showCarouselBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    let carousel1 = document.getElementById("carousel1")
+    carousel1.style.display = "block"
+    showCarouselBtn.style.display = "none"
+})
+
+let closeCarouselBtn = document.getElementById("close-carousel2")
+closeCarouselBtn.addEventListener("click", function () {
+    let carousel1 = document.getElementById("carousel1")
+    carousel1.style.display = "none"
+    let showCarouselBtn = document.getElementById("show-carousel")
+    showCarouselBtn.style.display = "inline"
+})
